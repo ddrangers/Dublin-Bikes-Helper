3ce6ad0692ea152132511375b3c34a821c354181
Jingao <jingao.liu13@gmail.com>
2023-04-07 18:57:37 +0100

    Update /availableInfo/${indexnumber} API

--- BackendService_DBH/app.py
+++ BackendService_DBH/app.py
@@ -1,13 +1,13 @@
 import json
 import requests
-from flask import Flask
+from flask import Flask, jsonify
 from sqlalchemy import create_engine, select, MetaData, Table, text
 import pandas as pd
-from flask_cors import CORS
+# from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 @app.route('/stations')
 def index():
@@ -122,7 +122,45 @@ def stations():
     return "list of stations"
 
 
-@app.route('/stations/<int:station_id>')
-def station(station_id):
-    # show the station with the given id, the id is an integer
-    return 'Retrieving info for Station: {}'.format(station_id)
+@app.route('/stations/<int:indexnumber>')
+def station(indexnumber):
+    dub_bike_response = requests.get(
+        'https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=004cf9dced9bc8d383db556938be65d9449aeae2')
+    if dub_bike_response.status_code == 200:
+        print('work')
+        bike_json = json.loads(dub_bike_response.text)
+        for station in bike_json:
+            if station['number'] == indexnumber:
+                # Extract the required fields for the station
+                index = station['number']
+                name = station['name']
+                address = station['address']
+                bike_stand = station['bike_stands']
+                bike_stand_available = station['available_bike_stands']
+                bike_available = station['available_bikes']
+                status = station['status']
+
+                # Create a dictionary to store the extracted data
+                data = {
+                    "index": index,
+                    "name": name,
+                    "address": address,
+                    "bike_stand": bike_stand,
+                    "bike_stand_available": bike_stand_available,
+                    "bike_available": bike_available,
+                    "status": status
+                }
+
+                # Convert the dictionary to JSON and return the result #in fuction add return
+                station_result = json.dumps(data, indent=3)  # # Convert the dictionary to JSON
+                return station_result
+            # Return an error message if the station ID is not found
+        data = "error: Station not found"
+        station_result = json.dumps(data, indent=3)
+        return station_result
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
